f02bc34e195e137bddfb95b1c3be400e5090423c
hichembou21 <hichem.bouaffar@gmail.com>
2017-12-21 14:55:38 +0100

    correction des premiers exercices

--- www/app.js
+++ www/app.js
@@ -1,49 +1,71 @@
-// let v=0;
-// let j=1;
-// let u=1;
+let v=0;
+let j=1;
 
-
+// exercice 1 : Ecrire dans la console tous les chiffres de 1 à 10
 // for (let i = 1; i <= 10; i++) {
 //     console.log(i);
 // }
 
+// exercice 2 : Ecrire dans la console tous les chiffres pairs de 1 à 10
 // for (let i = 0; i <= 10; i+=2) {
 //     console.log(i);
 // }
- 
-// for (let i = j; i <= v; i+=x) {
+
+// exercice 3 : Ecrire dans la console tous les chiffres avec choix 
+// de l'incrémentation de x à y
+
+// const step = 3;
+
+// for (let i = j; i <= 10; i+=step) {
 //     console.log(i);
 // }
 
 // console.log(v-1);
-// while (i < 1000) {
+// while (i < 100) {
 //     console.log(v);
 //     i=i+v;
 //     v=j;  
 //     j=i;  
 // }
 
-// ****** la suite de fibonacci
-// for (let i = 1; i <= 100; i++) {
-//     console.log(v);
-//     u=u+v;
-//     v=j;  
-//     j=u;  
-//  }
- const devine = 100; 
- let nombre=0;
- 
- while (nombre != devine) {
-
-    if (nombre == devine) {
-        alert("Bravo !! vous avez deviné le nombre magique");  
-    } else if (nombre > devine) {
-        nombre = parseInt(prompt("c'est moin essaye encore"));
-    } else {
-        nombre = parseInt(prompt("c'est plus essaye encore"));
+// ******  exercice 4 : la suite de fibonacci
+function suiteFibonacci(params=0) {
+    let table = [];
+    let u = 1;
+    if (params > 0) {
+        table.push(0);
     }
-     
+
+        for (let i = 0; i < params - 1; i++) {
+            table.push(u);
+            u += table[i];   
+        }
+        return table;
+}
+ 
+// exercice 5 : function jeu nombre à deviner ;) 
+
+function devineNumber(devine) {
+ 
+ let nombre = parseInt(prompt("Ya un nombre à deviner ! entre votre nombre"));
+ 
+ while (true)  { 
+
+    if (nombre < devine) {
+        nombre = parseInt(prompt("c'est plus essaye encore"));
+    } if (nombre > devine) {
+        nombre = parseInt(prompt("c'est moin essaye encore"));
+    } if (nombre === devine) {
+        alert("Bravo !! vous avez deviné le nombre magique");
+        break;
+    } else {
+        nombre = parseInt(prompt("il faut entrer un nombre !!!"));
+    }
  }
+}
+
+//console.log(suiteFibonacci(100));
+devineNumber(69);
 
 
 
